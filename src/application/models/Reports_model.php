cb709d6fd785606c47a625375bb490affce57ecb
stratosilva <y17V8p4769>
2017-04-07 16:04:41 +0200

    Modified query in Records model to avoid duplicate records showing and also to include empty categories

--- src/application/models/Reports_model.php
+++ src/application/models/Reports_model.php
@@ -11,13 +11,13 @@ class Reports_model extends CI_Model
 	public function getAll()
 	{
 		return $this->db->query('
-			SELECT ap.start_datetime start, sc.name category_name, se.name service_name, CONCAT(us.first_name, \' \', us.last_name) provider_name,
+                        SELECT distinct ap.start_datetime start, IFNULL(sc.name, \'\') category_name, se.name service_name, CONCAT(us.first_name, \' \', us.last_name) provider_name,
 				CONCAT(cu.first_name, \' \', cu.last_name) customer_name, cu.gender, cu.address, cu.phone_number_1,
-				cu.country_origin, cu.language, (IF(ap.end_datetime < NOW(), ( IF(ap.no_show > 0, \'Not Show\', \'Seen\')), \'Scheduled\')) as status
+				cu.country_origin, cu.language, (IF(ap.end_datetime < NOW(), ( IF(ap.no_show > 0, \'No Show\', \'Seen\')), \'Scheduled\')) as status
 			FROM ea_appointments ap
 				INNER JOIN ea_services se ON ap.id_services = se.id
 				INNER JOIN ea_services_providers sp ON ap.id_services = sp.id_services
-				INNER JOIN ea_service_categories sc ON se.id_service_categories = sc.id
+				LEFT JOIN ea_service_categories sc ON se.id_service_categories = sc.id
 				INNER JOIN ea_customers cu ON ap.id_users_customer = cu.id
 				INNER JOIN ea_users us ON ap.id_users_provider = us.id ')
 		->result();
